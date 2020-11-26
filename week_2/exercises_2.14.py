a9c5d2aad4ec63a0e071beb114baf0c41cc1b9de
Your Name <you@example.com>
2020-11-26 14:17:58 +0100

    week_2_exercises

---
+++ week_2/exercises_2.14.py
@@ -0,0 +1,27 @@
+#exercises week 2
+
+word_1 = "All"
+word_2 = "work"
+word_3 = "and"
+word_4 = "no"
+word_5 = "play"
+word_6 = "make"
+word_7 = "Jack"
+word_8 = "a"
+word_9 = "dull"
+word_10 = "boy"
+
+print(word_1, word_2, word_3, word_4, word_5, word_6, word_7, word_8, word_9, word_10)
+
+print(6*(1-2))
+
+bruce = 6
+print(bruce+4)
+
+final_amount =
+P = principal amount
+r = annual nominal interst rate
+n = number of times the interest is compounded per year
+t = number of years
+
+A = P (1+(r/n)^n*t
