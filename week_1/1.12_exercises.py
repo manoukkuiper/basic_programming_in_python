fc4bd54ec4cddbc70aab70dce654346127268f9f
Your Name <you@example.com>
2020-11-26 11:00:37 +0100

    Added a git .ignore file

---
+++ week_1/1.12_exercises.py
@@ -0,0 +1,5 @@
+# Write an English or Dutch sentence with understandable semantics but incorrect syntax.
+# Write an English or Dutch sentence which has correct syntax but has semantic errors.
+
+# Type 1 + 2
+1 + 2
